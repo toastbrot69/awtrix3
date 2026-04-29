c64f6dd1e5486cef85d1063cf1d9ac7d192ce183
Pohlmann <pohlmann@tetronik.com>
2026-04-29 17:05:11 +0200

    webseite

--- src/LedMatrix-HUB75.cpp
+++ src/LedMatrix-HUB75.cpp
@@ -8,7 +8,7 @@ void GenericLedMatrixIF::clear()
 }
 
 void GenericLedMatrixIF::show()
-{
+{ 
 }
 
 void GenericLedMatrixIF::drawBitmap(int16_t x, int16_t y, const uint8_t bitmap[], int16_t w, int16_t h, uint16_t color)
